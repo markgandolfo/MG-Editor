8d57e0c9cb87766493aee7e2750846d95d92df3e
Mark Gandolfo <mark@gandolfo.com.au>
2017-01-11 17:43:53 +1100

    Refactor: NoteListItem into .vue file.

---
+++ src/components/NoteListItem/NoteListItem.vue
@@ -0,0 +1,56 @@
+<template>
+  <li v-bind:class='{ active: isActive }'>
+    <i class='close pull-right' title='delete' v-on:click='deleteNote'>x</i>
+    <a href='#' v-on:click='openNote'>
+      <div class='date'>
+        {{ note.prettyDate() }}
+      </div>
+      <span>{{ note.title() }}</span>
+    </a>
+  </li>
+</template>
+
+<script>
+import swal from 'sweetalert';
+
+export default {
+  props: ['note'],
+
+  computed: {
+    isActive: function () {
+      return this.note.id === this.$parent.note.id;
+    }
+  },
+
+  methods: {
+    openNote: function () {
+      const clickedNoteId = parseInt(this.note.id, 10);
+      this.$parent.note = _.find(this.$parent.notesList, { id: clickedNoteId });
+    },
+
+    deleteNote: function () {
+      const clickedNoteId = parseInt(this.note.id, 10);
+      const loadedNote = this.$parent.note.id;
+
+      swal({
+        title: 'Deleting',
+        text: 'Are you sure you would like to delete?',
+        cancelButtonText: 'No',
+        type: 'error',
+        showCancelButton: true,
+        confirmButtonColor: '#DD6B55',
+        confirmButtonText: 'Yes',
+        closeOnConfirm: true
+      }, () => {
+        Note.delete(clickedNoteId, () => {
+          this.$parent.updateNoteList(() => {
+            if (loadedNote === clickedNoteId) {
+              this.$parent.loadFirstNote();
+            }
+          });
+        });
+      });
+    }
+  },
+}
+</script>
--- src/js/app.js
+++ src/js/app.js
@@ -7,68 +7,20 @@ import DragResizer from 'js/DragResizer';
 import Note from 'models/Note';
 import Vue from 'vue';
 import marked from 'marked';
-import swal from 'sweetalert';
 import LoadingScreen from 'components/LoadingScreen/LoadingScreen';
+import NoteItem from 'components/NoteListItem/NoteListItem.vue';
 
 const autoResizer = new AutoResizer('auto-heighter');
 autoResizer.resize();
 autoResizer.autoResizeOnWindowResize();
 
-Vue.component('note-item', {
-  template: `
-    <li v-bind:class='{ active: isActive }'>
-      <i class='close pull-right' title='delete' v-on:click='deleteNote'>x</i>
-      <a href='#' v-on:click='openNote'>
-        <div class='date'>
-          {{ note.prettyDate() }}
-        </div>
-        <span>{{ note.title() }}</span>
-      </a>
-    </li>
-  `,
-
-  props: ['note'],
-
-  computed: {
-    isActive: function () {
-      return this.note.id === this.$parent.note.id;
-    }
-  },
-
-  methods: {
-    openNote: function () {
-      const clickedNoteId = parseInt(this.note.id, 10);
-      this.$parent.note = _.find(this.$parent.notesList, { id: clickedNoteId });
-    },
-
-    deleteNote: function () {
-      const clickedNoteId = parseInt(this.note.id, 10);
-      const loadedNote = this.$parent.note.id;
-
-      swal({
-        title: 'Deleting',
-        text: 'Are you sure you would like to delete?',
-        cancelButtonText: 'No',
-        type: 'error',
-        showCancelButton: true,
-        confirmButtonColor: '#DD6B55',
-        confirmButtonText: 'Yes',
-        closeOnConfirm: true
-      }, () => {
-        Note.delete(clickedNoteId, () => {
-          this.$parent.updateNoteList(() => {
-            if (loadedNote === clickedNoteId) {
-              this.$parent.loadFirstNote();
-            }
-          });
-        });
-      });
-    }
-  },
-});
-
 new Vue({ // eslint-disable-line no-new
   el: '#main-content',
+
+  components: {
+    NoteItem
+  },
+
   data: {
     input: '',
     note: null,
